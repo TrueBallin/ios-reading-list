98fc3b114540fbe94101cccb027aa585dc85d988
TrueBallin <ballin_true@yahoo.com>
2020-02-14 15:29:44 -0800

    Reading list.

---
+++ Reading List/Reading List/BookController.swift
@@ -0,0 +1,17 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by True Ballin on 2/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+
+class BookController {
+    var books: [Book] = []
+}
+
+
